3396f9490d45d12a0831871b59ecadd28474069e
Jakub Postępski <jakub.postepski@gmail.com>
2020-10-25 12:44:22 +0100

    add app with benford detection

--- app.py
+++ app.py
@@ -1,9 +1,65 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from benford import detect_benford
+import json
 
-@app.route('/')
+app = Flask(__name__)
+UPLOAD_FOLDER = '/tmp'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            resp = detect_benford(filepath)
+            print(resp)
+            file.save(filepath)
+
+            return redirect(url_for('answer',
+                                    analisys=json.dumps(resp)))
+    return render_template('index.html')
+
+
+@app.route('/answer', methods=['GET', 'POST'])
+def answer():
+    resp = {}
+    try:
+        resp = json.loads(str(request.args.get("analisys")))
+    except:
+        return "error loading get args"
+
+    color_palette = ["rgba(255, 241, 0, 0.2)", "rgba(255, 140, 0, 0.2)", "rgba(232, 17, 35, 0.2)",
+                     "rgba(0, 158, 73, 0.2)", "rgba(186, 216, 10, 0.2)",
+                     "rgba(0, 24, 143, 0.2)", "rgba(0, 188, 242, 0.2)", "rgba(0, 178, 148, 0.2)",
+                     "rgba(236, 0, 140, 0.2)", "rgba(104, 33, 122, 0.2)"
+                     ]
+
+    if len(color_palette) < len(resp["columns_distribution"]):
+        return "to many columns to display"
+
+    return render_template("answer.html", resp=resp, cols_len=len(resp["columns_distribution"]), color_palette=color_palette)
+
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
---
+++ benford.py
@@ -0,0 +1,53 @@
+from functools import reduce
+
+def detect_benford(filename):
+    register = []
+
+    def insert_into_register(leading_digit, i):
+        while len(register) <= i:
+            register.append([0 for x in range(10)])
+
+        register[i][leading_digit] = register[i][leading_digit] + 1
+
+    columns_count = 0
+    correct_insertion = True
+    first_line = True
+    with open(filename) as fp:
+        Lines = fp.readlines()
+        for line in Lines:
+            if first_line:
+                first_line = False
+                continue
+
+            split_line = line.replace("\n", "").split("\t")
+            for i, arg in enumerate(split_line):
+                arg = arg.replace(" ", "")
+                converted = None
+                try:
+                    converted = float(arg)
+                except:
+                    pass
+
+                if isinstance(converted, (float, int)):
+                    if converted >= 0:
+                        leading_digit = int(arg[0])
+                    else:
+                        leading_digit = int(arg[1])
+                    insert_into_register(leading_digit, i)
+            if columns_count:
+                correct_insertion = columns_count == len(split_line)
+            else:
+                columns_count = len(split_line)
+                correct_insertion = True
+
+    records_count = [sum(column) for column in register]
+    benford_detected = [col_sum > 10 and col[1] / col_sum > 0.25 and col[1] / col_sum < 0.35 for (col, col_sum) in
+                        zip(register, records_count)]
+
+    return ({"benford_detect": benford_detected,
+             "columns_distribution": register,
+             "records_count": records_count,
+             "correct_insertion": correct_insertion})
+
+if __name__ == '__main__':
+    print(detect_benford("census_2009b"))
